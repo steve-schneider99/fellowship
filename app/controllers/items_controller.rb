4e01b29e4faef17b26e62b0d6e7a076bf98314f7
Steve <steve@tripgrid.com>
2015-11-22 00:02:47 -0800

    add base code for adding items, a super basic form

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,28 @@
 class ItemsController < ApplicationController
 
-def index
+  def index
+    @items = Item.all
+  end
 
-end
+  def new
+    @item = Item.new
+  end
 
+  def create
+    item =  Item.new(item_params)
+    if item.save
+      flash[:notice] = "Item was created!"
+      redirect_to items_path
+    else
+      flash[:alert] = "Error while submitting."
+      render :new
+    end
+  end
+
+private
+
+  def item_params
+    params.require(:item).permit(:name)
+  end
 
 end
