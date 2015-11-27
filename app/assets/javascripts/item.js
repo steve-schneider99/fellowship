bb09d2160d2cf8fadb3ff17510c7a7b05a9cb985
Steve <steve@tripgrid.com>
2015-11-27 12:43:29 -0800

    add code for social page(plan to integrate facebook app at a later point). Also added some prelim javascript/jquery to be amended later when I get into functionality.

---
+++ app/assets/javascripts/item.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $('h5').click(function() {
+    alert("JQuery is working!");
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users
   resources :items
+  resource :home do
+    member do
+      get 'social', to: "home#social"
+    end
+  end
   root to: "home#homepage"
 
 
